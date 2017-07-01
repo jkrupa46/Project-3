06d5c7a551056829c4b524e492e0e23f4a133b7f
Jacob <jkrupa46@gmail.com>
2017-07-01 02:51:34 -0500

    added some more javascript features to the google maps api

--- script.js
+++ script.js
@@ -19,11 +19,27 @@ function initMap()
         position: google.maps.ControlPosition.RIGHT_CENTER
       }
     });
+    
+    var contentString = 
+      '<h1>Etihad Stadium</h1>'+
+      '<p><b>Etihad Stadium</b>, also known as the City of Manchester Stadium, in Manchester, England, is the home ground of Manchester City Football Club and, with a domestic football capacity of 55,097, the fourth-largest in the Premier League and eighth-largest in the United Kingdom.</p>'+
+      '<p>For more information, click on this link: <a href="https://en.wikipedia.org/wiki/City_of_Manchester_Stadium"></p>;
+        
+    var infowindow = new google.maps.InfoWindow(
+    {
+      content: contentString
+    });
+    
     var marker = new google.maps.Marker(
     {
       position: {lat: 53.483137, lng: -2.200404},
       map: map,
-      title: 'Etihad Stadium',
+      title: 'Etihad Stadium'
+    });
+    
+    marker.addListener('click', function() 
+    {
+      infowindow.open(map, marker);
     });
 }
 
