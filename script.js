2908078d0594cf46c47c64a0ed7b1591f347a1b0
Jacob <jkrupa46@gmail.com>
2017-07-01 01:30:57 -0500

    added some  extra javascript options to the map

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 var map;
 function initMap() 
 {
-    alert("ready");
     map = new google.maps.Map(document.getElementById('map'), 
     {
       center: {lat: 53.483137, lng: -2.200404},
-      zoom: 8
+      zoom: 18,
+      mapTypeId: 'satellite',
+      mapTypeControl: true,
+      mapTypeControlOptions: 
+      {
+        style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+        mapTypeIds: ['roadmap', 'satellite'],
+        position: google.maps.ControlPosition.TOP_CENTER
+      },
+      zoomControl: true,
+      zoomControlOptions: 
+      {
+        position: google.maps.ControlPosition.RIGHT_CENTER
+      }
+    });
+    var marker = new google.maps.Marker(
+    {
+      position: {lat: 53.483137, lng: -2.200404},
+      map: map,
+      title: 'Etihad Stadium',
+      icon: 'images/googlemapsicon'
     });
 }
 
